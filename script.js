8df0ad2d7a349bf6a04afe7bfe982404e069bf67
Cindy Vidale <52414957+ArsenicNepenthes@users.noreply.github.com>
2019-10-30 15:51:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function getRandomInt (){
+    var list=[
+    "ANNICCHIARICO Victoria",
+    "RIEKER Théo",
+    "CLEMENTZ Nathan",
+    "JBARA Naïm",
+    "MOUGIN Mehdi",
+    "ALAMANOVA Madina",
+    "BLANGENWITSCH Lucas",
+    "RÖDERER Lucas",
+    "LUCZAK Justin",
+    "LAT Jordan",
+    "LIN Joël",
+    "CHÂTEL Jérémy",
+    "ROCFORT Erwan",
+    "ZEMB Edouard",
+    "WITTNER Déborah",
+    "DE OLIVEIRA Allan",
+    "BIRLINGER Anaïs",
+    "MAERKY Antonin",
+    "LINDER Arnaud",
+    "FRUHAUF Thibaud",
+    "VIDALE Cindy",
+    "ALALI Mouafak",
+    "BASS Kévin",
+    "BARBIER Damien"
+]; 
+    var max = list.length;
+    var generateur = Math.floor(Math.random()*24)
+
+  }
