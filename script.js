889323edcb42c69e8a428a8d5e5137d6f62a7a6b
Cindy Vidale <52414957+ArsenicNepenthes@users.noreply.github.com>
2019-10-31 09:23:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-function getRandomInt (){
+function getRandom (){
     var list=[
-    "ANNICCHIARICO Victoria",
-    "RIEKER Théo",
-    "CLEMENTZ Nathan",
-    "JBARA Naïm",
-    "MOUGIN Mehdi",
-    "ALAMANOVA Madina",
-    "BLANGENWITSCH Lucas",
-    "RÖDERER Lucas",
-    "LUCZAK Justin",
-    "LAT Jordan",
-    "LIN Joël",
-    "CHÂTEL Jérémy",
-    "ROCFORT Erwan",
-    "ZEMB Edouard",
-    "WITTNER Déborah",
-    "DE OLIVEIRA Allan",
-    "BIRLINGER Anaïs",
-    "MAERKY Antonin",
-    "LINDER Arnaud",
-    "FRUHAUF Thibaud",
-    "VIDALE Cindy",
-    "ALALI Mouafak",
-    "BASS Kévin",
-    "BARBIER Damien"
-]; 
-    var max = list.length;
-    var generateur = Math.floor(Math.random()*24)
+        "ANNICCHIARICO Victoria",
+        "RIEKER Théo",
+        "CLEMENTZ Nathan",
+        "JBARA Naïm",
+        "MOUGIN Mehdi",
+        "ALAMANOVA Madina",
+        "BLANGENWITSCH Lucas",
+        "RÖDERER Lucas",
+        "LUCZAK Justin",
+        "LAT Jordan",
+        "LIN Joël",
+        "CHÂTEL Jérémy",
+        "ROCFORT Erwan",
+        "WITTNER Déborah",
+        "DE OLIVEIRA Allan",
+        "BIRLINGER Anaïs",
+        "MAERKY Antonin",
+        "LINDER Arnaud",
+        "FRUHAUF Thibaud",
+        "VIDALE Cindy",
+        "ALALI Mouafak",
+        "BASS Kévin",
+        "BARBIER Damien"
+    ]; 
+var max = list.length;
+var generateur = Math.floor(Math.random()*max)
+console.log(list[generateur]);
 
-  }
+}
