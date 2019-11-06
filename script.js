079dfbf1ee4ad6054cad2b991295cbdecfc57c85
Cindy Vidale <52414957+ArsenicNepenthes@users.noreply.github.com>
2019-11-06 23:34:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-function getRandom (){
-    var list=[
-        "ANNICCHIARICO Victoria",
-        "RIEKER Théo",
-        "CLEMENTZ Nathan",
-        "JBARA Naïm",
-        "MOUGIN Mehdi",
-        "ALAMANOVA Madina",
-        "BLANGENWITSCH Lucas",
-        "RÖDERER Lucas",
-        "LUCZAK Justin",
-        "LAT Jordan",
-        "LIN Joël",
-        "CHÂTEL Jérémy",
-        "ROCFORT Erwan",
-        "WITTNER Déborah",
-        "DE OLIVEIRA Allan",
-        "BIRLINGER Anaïs",
-        "MAERKY Antonin",
-        "LINDER Arnaud",
-        "FRUHAUF Thibaud",
-        "VIDALE Cindy",
-        "ALALI Mouafak",
-        "BASS Kévin",
-        "BARBIER Damien"
-    ]; 
-var max = list.length;
-var generateur = Math.floor(Math.random()*max)
-console.log(list[generateur]);
+function pickRandomFighterInAList(listOfFighters) {
+  var max = listOfFighters.length;
+  var generateur = Math.floor(Math.random() * max);
+  return listOfFighters[generateur];
+}
 
+function removeFighterFromList(figther, listOfFighters) {
+  const indexOf = listOfFighters.indexOf(figther);
+  return listOfFighters.splice(indexOf, 1);
+}
+
+function pickNFighters(numberOfFigthers = 2) {
+  let listOfFighters = [
+    "ANNICCHIARICO Victoria",
+    "RIEKER Théo",
+    "CLEMENTZ Nathan",
+    "JBARA Naïm",
+    "MOUGIN Mehdi",
+    "ALAMANOVA Madina",
+    "BLANGENWITSCH Lucas",
+    "RÖDERER Lucas",
+    "LUCZAK Justin",
+    "LAT Jordan",
+    "LIN Joël",
+    "CHÂTEL Jérémy",
+    "ROCFORT Erwan",
+    "WITTNER Déborah",
+    "DE OLIVEIRA Allan",
+    "BIRLINGER Anaïs",
+    "MAERKY Antonin",
+    "LINDER Arnaud",
+    "FRUHAUF Thibaud",
+    "VIDALE Cindy",
+    "ALALI Mouafak",
+    "BASS Kévin",
+    "BARBIER Damien"
+  ];
+
+  const figthers = [];
+  for (let i = 0; i < numberOfFigthers; i++) {
+    const selectedFigther = pickRandomFighterInAList(listOfFighters);
+    removeFighterFromList(selectedFigther, listOfFighters);
+    figthers.push(selectedFigther);
+  }
+
+  return figthers;
+}
+
+function displayNFigthers(numberOfFigthers) {
+  const fighters = pickNFighters(numberOfFigthers);
+
+  let fightName = "";
+  for (let i = 0; i < fighters.length; i++) {
+    fightName += fighters[i];
+    if (i < fighters.length - 1) {
+      fightName += " vs ";
+    }
+  }
+
+  const element = document.getElementById("fighters");
+  element.innerHTML = fightName;
 }
